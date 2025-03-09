ee4c347069a008d65a421a878eccfaec74ff8941
Ricky Wirawan <rickywrwn@gmail.com>
2025-03-09 16:41:31 +0700

    Initial Commit

---
+++ UbersnapTemperature/UbersnapTemperatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  UbersnapTemperatureApp.swift
+//  UbersnapTemperature
+//
+//  Created by ricky wirawan on 09/03/25.
+//
+
+import SwiftUI
+
+@main
+struct UbersnapTemperatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
